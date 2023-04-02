aa9dbf43c10ea228b7ce05a20199a18fac2c492f
hori <fourbasic19@gmail.com>
2023-04-02 17:19:39 +0900

    【CRUD】post機能を追加

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const app = express();
 const frequentData = require('./frequent.json'); // frequent.jsonファイルを読み込む
 
 app.use(cors()); // CORSを有効にする
+app.use(express.json()); // JSONデータを解析するために必要なミドルウェアを追加
 
 app.get('/api/frequent', (req, res) => {
   res.json(frequentData.data); // frequentDataオブジェクトのdataプロパティを返す
+});
+
+app.post('/api/frequent', (req, res) => {
+  const newData = req.body; // リクエストボディから新しいデータを取得
+  frequentData.data.push(newData); // frequentDataオブジェクトのdataプロパティに新しいデータを追加
+  res.json({ message: 'Data added successfully' }); // レスポンスとして追加が完了したことを通知する
 });
 
 const port = 3000; // ポート番号を指定
