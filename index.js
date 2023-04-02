8e733ebc623f463fc8a69c9e94fe3a0497a44a42
hori <fourbasic19@gmail.com>
2023-04-02 18:43:34 +0900

    新規用語にID番号を付与

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ app.get('/api/frequent', (req, res) => {
 
 app.post('/api/frequent', (req, res) => {
   const newData = req.body; // リクエストボディから新しいデータを取得
+  const lastId = frequentData.data[frequentData.data.length - 1].id; // 最後尾のオブジェクトのIDを取得
+  const newId = lastId + 1; // 新しいIDを生成
+  newData.id = newId; // 新しいデータのIDプロパティに新しいIDを設定
   frequentData.data.push(newData); // frequentDataオブジェクトのdataプロパティに新しいデータを追加
-  res.json({ message: 'Data added successfully' }); // レスポンスとして追加が完了したことを通知する
+  res.json({ message: 'Data added successfully', newId }); // レスポンスとして追加が完了したことと、新しいIDを返す
 });
 
 const port = 3000; // ポート番号を指定
